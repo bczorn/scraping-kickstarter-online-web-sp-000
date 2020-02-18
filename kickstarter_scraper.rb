010fce2d704b2d20feaf81f06472b0f2ac82a8af
Becky Zorn <bczorn@gmail.com>
2020-02-18 16:53:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 def create_project_hash
   
+  # this opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
